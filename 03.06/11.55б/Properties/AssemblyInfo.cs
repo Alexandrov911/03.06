1464ff30a888a4d26b4e57b1651ad01b01cb8995
Alexandrov911 <sashok17062002@mail.ru>
2021-06-08 17:14:31 +0300

    03.06

---
+++ 03.06/11.55б/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("11.55б")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("11.55б")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5470cc78-0561-441c-8692-6b9d79ad3a4d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 03.06/11.55в/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._55в
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //11.55в
+            Console.WriteLine("a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int n = 10;
+            int[] mas = new int[n];
+            int sum = 0;
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(-20, 20);
+                Console.WriteLine(mas[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] % a == 0 || mas[i] % b == 0)
+
+                {
+                    sum = sum + mas[i];
+                }
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
